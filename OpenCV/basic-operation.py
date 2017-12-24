b4b4beac2b72f772ebfde5f7670f23be5dab904c
Yeharold <1174484433@qq.com>
2017-12-24 20:51:12 +0800

    first

---
+++ OpenCV/basic-operation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+# @Date    : 2017-12-24 20:34:24
+# @Author  : YeHarold (1174484433@qq.com)
+# @Link    : https://github.com/Yeharold
+
+import cv2
+from numpy as np 
+
+img = cv2.imread('')
